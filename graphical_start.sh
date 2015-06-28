d5d2bce0c7dc092c6737d4f3eaa1f02e4892ee00
BlackNoxis <steven.darklight@gmail.com>
2015-06-28 11:21:58 +0300

    refacut graphical_start.sh intr-o forma mai optima

--- graphical_start.sh
+++ graphical_start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-setup_displaymanager() {
-        # determine what is the login manager
-        if [ -n "$(equo match --installed gnome-base/gdm -qv)" ]; then
-                systemctl start gdm
-        elif [ -n "$(equo match --installed lxde-base/lxdm -qv)" ]; then
-                systemctl start lxdm
-        elif [ -n "$(equo match --installed x11-misc/lightdm-base -qv)" ]; then
-                systemctl start lightdm
-        elif [ -n "$(equo match --installed kde-base/kdm -qv)" ]; then
-                systemctl start kdm
-        elif [ -n "$(equo match --installed x11-misc/slim -qv)" ]; then
-                systemctl start slim
-        else
-                systemctl start xdm
-        fi
-}
+
+if [ -n "$(equo match --installed gnome-base/gdm -qv)" ]; then
+	systemctl start gdm
+elif [ -n "$(equo match --installed lxde-base/lxdm -qv)" ]; then
+	systemctl start lxdm
+elif [ -n "$(equo match --installed x11-misc/lightdm-base -qv)" ]; then
+	systemctl start lightdm
+elif [ -n "$(equo match --installed kde-base/kdm -qv)" ]; then
+	systemctl start kdm
+elif [ -n "$(equo match --installed x11-misc/slim -qv)" ]; then
+	systemctl start slim
+else
+	systemctl start xdm
+fi
