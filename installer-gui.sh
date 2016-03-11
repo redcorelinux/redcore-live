2eb24611ef0c2bc838035951e5d27f663a58a820
V3n3RiX <venerix@rogentos.ro>
2016-03-11 19:45:03 +0000

    I really hate to be a developer right now...I have to rewrite this Sabayon inherited crap

--- installer-gui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /sbin/kogaion-functions.sh
-
-if kogaion_is_gui_install; then
-	kogaion_setup_autologin
-	kogaion_setup_gui_installer
-fi
--- installer-text.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /sbin/kogaion-functions.sh
-
-if kogaion_is_text_install; then
-	kogaion_setup_text_installer
-fi
