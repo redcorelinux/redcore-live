3102b7044fcc3c9862d1de29fb60f1c597826f7d
V3n3RiX <venerix@redcorelinux.org>
2017-07-15 18:17:04 +0100

    allow live user to play games out of-the-box

--- redcorelive-systemd.sh
+++ redcorelive-systemd.sh
@@ -21,7 +21,7 @@ redcore_add_live_user() {
 }
 
 redcore_live_user_groups() {
-	for group in tty disk lp wheel uucp console audio cdrom tape kvm cdemu video vboxsf vboxguest cdrw usb plugdev messagebus portage ; do
+	for group in tty disk lp wheel uucp console audio cdrom tape kvm cdemu games video vboxsf vboxguest cdrw usb plugdev messagebus portage ; do
 		gpasswd -a "$liveuser" "$group" > /dev/null 2>&1
 	done
 }
